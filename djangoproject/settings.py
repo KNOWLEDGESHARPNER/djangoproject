22b40af9d91f2f082956ca1c045eb4d4a1e7f2a1
KNOWLEDGESHARPNER <ashubig75@gmail.com>
2023-03-29 15:22:31 +0530

    final commit of part 1

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'playground',
     "debug_toolbar",
     'store',
+    'store_custom',
     'tags',
     'likes',
 ]
--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+admin.site.site_header='Storefront Admin'
+admin.site.index_title='Admin'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/',include('posts.urls')),
